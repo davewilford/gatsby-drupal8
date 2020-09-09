60c90cb43c8df34f3a1d8ff4cfe8503119f360f3
Dave Wilford <dave.wilford@capgemini.com>
2020-09-09 18:52:24 +0100

    Fixed issue with image rendering.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 import { graphql, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = ({data}) => (
@@ -22,7 +22,7 @@ const IndexPage = ({data}) => (
                 field_image {
                   localFile {
                     childImageSharp {
-                      fluid(maxWidth: 400, quality: 100) {
+                      fluid(maxWidth: 600, quality: 100) {
                         ...GatsbyImageSharpFluid
                       }
                     }
@@ -42,7 +42,7 @@ const IndexPage = ({data}) => (
             <h3><Link to={ edge.node.id }>{ edge.node.title }</Link></h3>
             <small><em>{ Date(edge.node.created) }</em></small>
             <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, width: `100%` }}>
-              <Image fluid={ edge.node.relationships.field_image.localFile.childImageSharp.fluid } />
+              <Img fluid={ edge.node.relationships.field_image.localFile.childImageSharp.fluid } />
             </div>
             <div dangerouslySetInnerHTML={{ __html: edge.node.body.value.split(' ').splice(0, 50).join(' ') + '...' }}></div>
           </>
