c0577568334b79cc6ecbb9c28252e076e5897002
Dave Wilford <dave.wilford@capgemini.com>
2020-09-09 19:56:30 +0100

    Updated header bg color.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#DC3232`,
       marginBottom: `1.45rem`,
     }}
   >
