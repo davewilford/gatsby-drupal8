349222923a1e81da2924158ab320a6d64c382881
Dave Wilford <dave.wilford@capgemini.com>
2020-09-09 18:12:04 +0100

    Update to default Gatsby project and use drupal source.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Drupal 8 sandbox`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -11,6 +11,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `http://dev-drupal8-gatsby.pantheonsite.io/`,
+        apiBase: `jsonapi`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,55 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+const IndexPage = ({data}) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allNodeArticle {
+          edges {
+            node {
+              title
+              body {
+                value
+              }
+              created
+              relationships {
+                field_image {
+                  localFile {
+                    childImageSharp {
+                      fluid(maxWidth: 400, quality: 100) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        {data.allNodeArticle.edges.map(edge => (
+          <>
+            <h3><Link to={ edge.node.id }>{ edge.node.title }</Link></h3>
+            <small><em>{ Date(edge.node.created) }</em></small>
+            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, width: `100%` }}>
+              <Image fluid={ edge.node.relationships.field_image.localFile.childImageSharp.fluid } />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: edge.node.body.value.split(' ').splice(0, 50).join(' ') + '...' }}></div>
+          </>
+        ))}
+      </Layout>
+    )}
+  />
 )
 
 export default IndexPage
